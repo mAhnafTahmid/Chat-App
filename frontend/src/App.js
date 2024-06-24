52a1ba36a5f0bd60b765771d5dd747497cd77b21
Ahnaf Tahmid <ahnaf@tahmid-20101555.local>
2024-06-25 01:05:47 +0600

    added a lot of components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import Login from "./pages/login/Login.js";
+import SignUp from "./pages/signup/SignUp.js";
+import Home from "./pages/home/Home.js";
 
 function App() {
   return (
     <div className="p-4 h-screen flex items-center justify-center">
-      <Login/>
+      <Home/>
     </div>
   );
 }
