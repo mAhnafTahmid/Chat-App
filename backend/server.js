43087a8fb95760446fc3fcbee89667b78f97dd7b
Ahnaf Tahmid <ahnaf@tahmid-20101555.local>
2024-06-26 01:08:41 +0600

    connected signup and logout with backend

--- backend/server.js
+++ backend/server.js
@@ -1,33 +1,28 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import cookieParser from 'cookie-parser'
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
-import authRoutes from './routes/auth.routes.js'
-import messageRoutes from './routes/message.routes.js'
-import userRoutes from './routes/user.routes.js'
+import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
 
+import connectToMongoDB from "./db/connectToMongoDB.js";
 
-import connectToMongoDB from './db/connectToMongoDB.js'
+dotenv.config();
 
-dotenv.config()
+const app = express();
+const PORT = process.env.PORT || 8000;
 
-const app = express()
-const PORT = process.env.PORT || 3501
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
 
-
-
-
-app.use(express.json())
-app.use(cookieParser())
-
-app.use('/api/auth', authRoutes)
-app.use('/api/messages', messageRoutes)
-app.use('/api/users', userRoutes)
-
-
-
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
 
 app.listen(PORT, () => {
-    connectToMongoDB()
-    console.log(`Server Running at port ${PORT}.`)
-})
+  connectToMongoDB();
+  console.log(`Server Running at port ${PORT}.`);
+});
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,29 @@
+import Home from "./pages/home/Home.js";
 import Login from "./pages/login/Login.js";
 import SignUp from "./pages/signup/SignUp.js";
-import Home from "./pages/home/Home.js";
+import { Navigate, Route, Routes } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+import { useAuthContext } from "./context/AuthContext.js";
 
 function App() {
+  const { authUser } = useAuthContext();
   return (
     <div className="p-4 h-screen flex items-center justify-center">
-      <Home/>
+      <Toaster />
+      <Routes>
+        <Route
+          path="/"
+          element={authUser ? <Home /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/login"
+          element={authUser ? <Navigate to="/" /> : <Login />}
+        />
+        <Route
+          path="/signup"
+          element={authUser ? <Navigate to="/" /> : <SignUp />}
+        />
+      </Routes>
     </div>
   );
 }
